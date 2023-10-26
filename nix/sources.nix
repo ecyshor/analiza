9fe57004b0b4482c29c1c2d0f9b48c1b72c5c9b7
Nicu Reut <contact@nicu.dev>
2023-10-26 21:10:10 +0200

    update

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,6 +1,6 @@
 {
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs.git";
-    rev = "6ccc4a59c3f1b56d039d93da52696633e641bc71";
+    rev = "51d906d2341c9e866e48c2efcaac0f2d70bfd43e";
   };
 }
