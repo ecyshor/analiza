c77e23fe33438808226483aa8a4824069ff7dcda
Nicu Reut <contact@nicu.dev>
2025-11-02 23:27:19 +0100

    eslint

--- tracker/eslint.config.js
+++ tracker/eslint.config.js
@@ -1,7 +1,7 @@
-import globals from 'globals';
-import js from '@eslint/js';
+const globals = require('globals');
+const js = require('@eslint/js');
 
-export default [
+module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
