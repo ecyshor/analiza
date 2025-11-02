3eceb1f2dec9c7e7e39ea64a8f9ad0bb320a326b
Nicu Reut <contact@nicu.dev>
2025-11-02 23:24:33 +0100

    .

---
+++ tracker/eslint.config.js
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
