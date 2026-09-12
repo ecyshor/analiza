2a9dcc96e5bd5f1b707b15cd0787e03a3989dd6c
Nicu Reut <contact@nicu.dev>
2026-09-12 20:01:59 +0200

    ,

--- eye-admin/playwright.config.ts
+++ eye-admin/playwright.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:5174',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
