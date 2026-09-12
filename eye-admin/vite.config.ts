75ba6398703ff12a044184db35ca9a3a6e62ccb7
Nicu Reut <contact@nicu.dev>
2026-09-12 21:23:26 +0200

    allow react prefix

--- eye-admin/vite.config.ts
+++ eye-admin/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
         plugins: [react()],
-        envPrefix: 'REACT_APP',
+        envPrefix: ['VITE_', 'REACT_APP_'],
         server: {
             host: true,
             port: 5174
